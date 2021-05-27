7c578fd41686effe06ee88a9023a622e943d00e1
dimon19994 <dik19994@gmail.com>
2021-05-27 21:47:56 +0300

    Final changes

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import boto3
 import matplotlib.pyplot as plt
 import pandas as pd
 from botocore.exceptions import ClientError
-from flask import Flask, render_template, request, jsonify, url_for, redirect, send_from_directory
+from flask import Flask, render_template, request, jsonify, url_for, send_from_directory
 
 from config import AWS_SESSION_TOKEN, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
 
@@ -27,26 +27,11 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = "uploaded_users_files"
 
 
-# def s3_check_file(file_name, bucket, derectory):
-#     all_files = list(
-#         map(lambda i: i.key.split("/")[-1], list(s3_resource.Bucket(bucket).objects.filter(Prefix=f"{derectory}/"))))
-#     return file_name in all_files
-
-
 def s3_upload(file_name, bucket, object_name=None):
-    """Upload a file to an S3 bucket
 
-    :param file_name: File to upload
-    :param bucket: Bucket to upload to
-    :param object_name: S3 object name. If not specified then file_name is used
-    :return: True if file was uploaded, else False
-    """
-
-    # If S3 object_name was not specified, use file_name
     if object_name is None:
         object_name = file_name
 
-    # Upload the file
     try:
         response = s3_client.upload_file(file_name, bucket, object_name)
     except ClientError as e:
@@ -56,15 +41,6 @@ def s3_upload(file_name, bucket, object_name=None):
 
 
 def s3_download(file_name, bucket, step):
-    """Upload a file to an S3 bucket
-
-    :param file_name: File to upload
-    :param bucket: Bucket to upload to
-    :param object_name: S3 object name. If not specified then file_name is used
-    :return: True if file was uploaded, else False
-    """
-
-    # Upload the file
     folders = s3_client.list_objects(Bucket="project-files-kpi", Prefix=f"output_folders/{step}")
     file = folders['Contents'][-1]["Key"].split("/")[-1]
     try:
@@ -78,17 +54,6 @@ def s3_download(file_name, bucket, step):
 
 
 def add_emrfs_step(bucket_url, cluster_id, emr_client):
-    """
-    Add an EMRFS command as a job flow step to an existing cluster.
-
-    :param command: The EMRFS command to run.
-    :param bucket_url: The URL of a bucket that contains tracking metadata.
-    :param cluster_id: The ID of the cluster to update.
-    :param emr_client: The Boto3 Amazon EMR client object.
-    :return: The ID of the added job flow step. Status can be tracked by calling
-             the emr_client.describe_step() function.
-    """
-
     job_flow_step = {
         'Name': 'Spark application',
         'ActionOnFailure': 'CONTINUE',
@@ -115,8 +80,6 @@ def add_emrfs_step(bucket_url, cluster_id, emr_client):
 
 
 def usage_demo():
-    # Assumes the first waiting cluster has EMRFS enabled and has created metadata
-    # with the default name of 'EmrFSMetadata'.
     cluster = emr_client.list_clusters(ClusterStates=['WAITING'])['Clusters'][0]
     add_emrfs_step(
         's3://project-files-kpi', cluster['Id'], emr_client)
@@ -151,7 +114,6 @@ def project():
                 break
         s3_upload(f"uploaded_users_files/{file.filename}", "project-files-kpi", "input_data/input.csv")
         os.remove(f"uploaded_users_files/{file.filename}")
-        # res = s3_check_file("input.csv", "project-files-kpi", "input_data")
         return jsonify("EMR cluster is running. Please wait")
 
 
@@ -170,9 +132,8 @@ def project_load_check():
         }
         s3_resource.meta.client.copy(copy_source, "project-files-kpi", f"output_folders/{step}/{i}")
 
-        # # Delete the former object A
         s3_resource.Object("project-files-kpi", f"otput_data/{i}").delete()
-    return f"<a href='/results/{step}'>Follow the link to view the results</a>"
+    return f"<a href='/results/{step}' class='form-link'>Follow the link to view the results</a>"
 
 
 @app.route("/results", methods=['GET', 'POST'])
@@ -189,7 +150,6 @@ def results(step=None):
     else:
         folders = s3_client.list_objects(Bucket="project-files-kpi", Prefix=f"output_folders/{step}")
         file = folders['Contents'][1]["Key"].split("/")[-1]
-        # s3_download("downloaded_users_files/res.csv", "project-files-kpi")
         obj = s3_client.get_object(Bucket="project-files-kpi", Key=f"output_folders/{step}/{file}")
         df = pd.read_csv(io.BytesIO(obj['Body'].read()))
 
@@ -209,14 +169,9 @@ def results(step=None):
 
 @app.route("/download/<step>")
 def download(step):
-    s3_download("downloaded_users_files/res.csv", "project-files-kpi", step)
+    ("downloaded_users_files/res.csv", "project-files-kpi", step)
     return send_from_directory(directory="downloaded_users_files", path="res.csv")
 
 
 if __name__ == "__main__":
-    app.run()
-
-# if __name__ == '__main__':
-#     usage_demo()
-#     s3_upload("uploaded_users_files/food_establishment_data.csv", "project-files-kpi", "input_data/food_establishment_data.csv")
-#     s3_download("downloaded_users_files/res.csv", "project-files-kpi")
+    app.run(debug=True)
